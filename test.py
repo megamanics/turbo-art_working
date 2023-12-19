a6424043093d3ed64d4acc6f7ea878f0fc337b59
SKi <megamanics@users.noreply.github.com>
2023-12-18 20:22:13 -0800

    feat: Modify image processing and inference function in test.py
    This commit introduce changes to the image processing and inference function of the test.py file.
    Below are the highlights of these changes:
    🚀 We have imported 'os' and 'random' libraries for file handling and generating random integer respectively.
    📂 The file reading mechanism has been modified, now it directly opens image files instead of reading binary stream.
    🔍 In the 'inference' function, the output file naming is randomized for unique names and saves as '.png' instead of bytes stream.
    🖼️ Finally, the 'inference' function is updated to directly take file path and other parameters.
    
    Note: It also includes some formatting fixes to maintain PEP8 standard.

--- test.py
+++ test.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 from io import BytesIO
 import torch
+import os
 from diffusers import AutoencoderKL, AutoPipelineForImage2Image
 from diffusers.utils import load_image
 from PIL import Image
 from pathlib import Path
+import random
 from huggingface_hub import snapshot_download
 
 
@@ -21,52 +23,48 @@ def download_models():
     # vae is used for a inference speedup
     snapshot_download("madebyollin/sdxl-vae-fp16-fix", ignore_patterns=ignore)
 
+
 download_models()
 
 pipe = AutoPipelineForImage2Image.from_pretrained(
-            "stabilityai/sdxl-turbo",
-            torch_dtype=torch.float16,
-            device_map="auto",
-            variant="fp16",
-            vae=AutoencoderKL.from_pretrained(
-                "madebyollin/sdxl-vae-fp16-fix",
-                torch_dtype=torch.float16,
-                device_map="auto",
-            ),
-        )
-
-# We execute a blank inference since there are objects that are lazily loaded that
-# we want to start loading before an actual user query
-pipe(
-            "blank",
-            image=Image.new("RGB", (800, 1280), (255, 255, 255)),
-            num_inference_steps=1,
-            strength=1,
-            guidance_scale=0.0,
-            seed=42,
-        )
+    "stabilityai/sdxl-turbo",
+    torch_dtype=torch.float16,
+    device_map="auto",
+    variant="fp16",
+    vae=AutoencoderKL.from_pretrained(
+        "madebyollin/sdxl-vae-fp16-fix",
+        torch_dtype=torch.float16,
+        device_map="auto",
+    ),
+)
 
 
 def inference(image, prompt, num_iterations):
-        img_data_in = image.read()
+    init_image = Image.open(image).resize((512, 512))
+    # based on trial and error we saw the best results with 3 inference steps
+    # it had better generation results than 4,5,6 even though it's faster
+    num_inference_steps = int(num_iterations)
+    # note: anything under 0.5 strength gives blurry results
+    strength = 0.999 if num_iterations == 2 else 0.65
+    assert num_inference_steps * strength >= 1
 
-        init_image = load_image(Image.open(
-            BytesIO(img_data_in))).resize((512, 512))
-        # based on trial and error we saw the best results with 3 inference steps
-        # it had better generation results than 4,5,6 even though it's faster
-        num_inference_steps = int(num_iterations)
-        # note: anything under 0.5 strength gives blurry results
-        strength = 0.999 if num_iterations == 2 else 0.65
-        assert num_inference_steps * strength >= 1
+    image = pipe(
+        prompt,
+        image=init_image,
+        num_inference_steps=num_inference_steps,
+        strength=strength,
+        guidance_scale=0.0,
+        seed=42,
+    ).images[0]
 
-        image = pipe(
-            prompt,
-            image=init_image,
-            num_inference_steps=num_inference_steps,
-            strength=strength,
-            guidance_scale=0.0,
-            seed=42,
-        ).images[0]
+    image_name = os.path.basename(image)
+    base_name, ext = os.path.splitext(image_name)
+    prompt_words = prompt.split()[:5]
+    prompt_part = "_".join(prompt_words)
+    seed = random.randint(0, 10000)
 
-        byte_stream = BytesIO()
-        image.save(byte_stream, format="jpeg")
+    output_image = f"{prompt_part}_{base_name}_{seed}.png"
+    image.save(output_image)
+
+
+inference('frontend/src/lib/assets/abstract.png', 'person on the beach', 2)
